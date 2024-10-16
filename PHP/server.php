50d944997c91beaf775f5b457530bc9556622012
KitBoothroyd <kitboothroyd27@gmail.com>
2024-10-16 09:37:32 +0100

    prime calc functions

--- PHP/server.php
+++ PHP/server.php
@@ -2,4 +2,37 @@
     header('Access-Control-Allow-Origin: http://localhost:3000');
     $input = $_POST['input'];
     $key = $_POST['key'];
+
+    function isPrime($n) {
+        if ($n < 2)
+            return false;
+
+        for ($i = 2; $i < intval($n**0.5) + 1; $i++)
+            if ($n % $i == 0)
+                return false;
+
+        return true;
+    }
+
+    function findNthPrime($n) {
+        $count = 0;
+        $current = 1;
+
+        while ($count < $n) 
+            $current++;
+            if (isPrime($current)) 
+                $count++;
+
+        return $current;
+    }
+
+    function encrypt($stringToEncrypt, $encryptionKey) {
+        $inputChars = str_split($stringToEncrypt);
+        $keyChars = str_split($encryptionKey);
+
+        $encryptedString = array();
+        for ($i = 0; $i < count($inputChars); $i++)
+            $out = $i + ord($keyChars[$i % count($keyChars)]);
+            echo "$out <br/>";
+    }
 ?>
